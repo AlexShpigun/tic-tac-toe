730c4e354bb2aab60cfc84d27b6319278048ad4a
AlexShpigun <alexshpigun0@gmail.com>
2022-01-23 23:04:29 +0200

    dimensional of madness

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,26 @@
 public class Main {
     public static void main(String[] args) {
+        int[][] gameField = {
+                {1, 2, 2},
+                {1, 1, 2},
+                {1, 2, 2} };
 
-        int[] line1 = {1, 2, 2};
-        int[] line2 = {2, 1, 1};
-        int[] line3 = {1, 2, 0};
 
-        if ((line1[0] == 1 && line2[1] == 1 && line3[2] == 1) || (line1[2] == 1 && line2[1] == 1 && line3[0] == 1)) {
+        if ((gameField[0][0] == 1 && gameField[1][1] == 1 && gameField[2][2] == 1) || (gameField[0][2] == 1 && gameField[1][1] == 1 && gameField[2][0] == 1)) {
 
             System.out.println("winner winner");
-        } else if ((line1[0] == 2 && line2[1] == 2 && line3[2] == 2) || (line1[2] == 2 && line2[1] == 2 && line3[0] == 2)) {
+        } else if ((gameField[0][0] == 2 && gameField[1][1] == 2 && gameField[2][2] == 2) || (gameField[0][2] == 2 && gameField[1][1] == 2 && gameField[2][0] == 2)) {
             System.out.println("noup");
-        }else {
+        } else {
             System.out.println("draw");
         }
 
         /*---------------------------------------------------------------------*/
-        if ((line1[1] == 2 && line2[1] == 2 && line3[1] == 2 || (line2[0] == 2 && line2[1] == 2 && line2[2] == 2))){
+        if ((gameField[0][1] == 2 && gameField[1][1] == 2 && gameField[2][1] == 2 || (gameField[1][0] == 2 && gameField[1][1] == 2 && gameField[1][2] == 2))) {
             System.out.println("winner winner chicken dinner");
-        } else if ((line1[1] == 1 && line2[1] == 1 && line3[1] == 1 || (line2[0] == 1 && line2[1] == 1 && line2[2] == 1))){
+        } else if ((gameField[0][1] == 1 && gameField[1][1] == 1 && gameField[2][1] == 1 || (gameField[1][0] == 1 && gameField[1][1] == 1 && gameField[1][2] == 1))) {
             System.out.println("noup again");
-        }else {
+        } else {
             System.out.println("draw again");
         }
 
